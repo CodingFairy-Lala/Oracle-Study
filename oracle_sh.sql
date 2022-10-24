d75a7282db1efb986b41b2b0502fe4b2dda9e137
Hera J <97158923+CodingFairy-Lala@users.noreply.github.com>
2022-10-24 23:03:31 +0900

    Add files via upload

--- oracle_sh.sql
+++ oracle_sh.sql
@@ -140,5 +140,454 @@ from
     
 -- 변경된 내용을 실제 DB서버에 반영
 commit; -- 변경사항 적용
---rollback; -- 변경사항 취소
+rollback; -- 변경사항 취소
+
+
+--=====================================
+-- DQL
+--=====================================
+-- Data Query Language
+-- DML의 하위 개념. 테이블의 데이터를 검색하기 위한 SQL
+-- 0행 이상의 결과집합(Result Set)이 반환됨.
+-- 특정 행/특정 컬럼에 대해서 조회가 가능하다.
+-- 특정 컬럼 기준으로 정렬이 가능하다.
+
+
+/*
+    (5) select 컬럼... -> 조회하고자 하는 컬럼
+    (1) from 테이블명 -> 조회하고자 하는 테이블
+    (2) where 조건절 -> 조회하고자 하는 행을 필터링 할 수 있는 조건절
+    (3) group by 컬럼... -> 행 단위 그룹핑
+    (4) having 조건절 -> 그룹핑 된 결과에 대해 필터링
+    (6) order by 컬럼... -> 특정 컬럼 기준으로 정렬
+*/
+
+-- job에서 job name 컬럼만 출력
+select
+    job_name
+from
+    job;
+    
+-- department에서 내용 전체 출력
+select
+    *
+from
+    department;
+
+-- employee에서 이름, 이메일, 전화번호, 입사일 출력
+select
+    EMP_NAME, EMAIL, PHONE, HIRE_DATE
+from
+    employee;
+
+-- employee에서 사번, 이름, 급여 출력
+select
+    EMP_no, emp_name, salary
+from
+    employee;
+
+-- employee에서 급여가 250만원 이상인 사원들의 사번, 이름, 급여 출력
+select
+    *
+from
+    employee
+where
+    salary >= 2500000;
+
+
+-- employee에서 현재 근무중인 사원의 이름을 오름차순으로 출력 asc : 오름차순 / dexc : 내림차순
+select
+    *
+from
+    employee
+where
+    quit_yn = 'N'
+order by
+    emp_name asc;
+
+----------------------------------------------------------
+-- select
+----------------------------------------------------------
+-- 가상 컬럼(산술 연산)이 가능
+select
+    emp_name, salary, salary*12, 100
+from
+    employee;
+-- 별칭(alias) 가능
+-- as "별칭" : as 생략 가능, 쌍타옴표 생략 가능
+-- 숫자로 시작하거나, 공백/특수문자가 포함된 경우는 쌍다옴표를 반드시 작성해야 한다.
+select
+    emp_name as "이 름",
+    salary as 급여
+from
+    employee;
+
+-- null 값에 대한 처리
+-- null값과는 산술연산 (+ - * /) 할 수 없다.
+
+-- 보너스 포함 급여 조회
+select
+    emp_name, salary, bonus,
+    nvl(bonus, 0),
+    salary + (salary * nvl(bonus, 0)) "보너스 포함 급여"
+from
+    employee;
+
+-- nvl() null 처리 함수
+-- nvl(col, value) : col 값이 null인 경우, value 반환
+
+-- employee에서 사번, 사원명, 급여, 보너스, 연봉(보너스 적용) 출력
+select
+    emp_id, emp_name, salary, bonus,
+    (salary + (salary * nvl(bonus,0)))*12 "ANNUAL SALARY"
+from
+    employee;
+
+-- 중복값 제거 distinct
+-- 컬럼에 중복된 값을 한번씩만 표현. select 뒤에 단 한번만 사용
+select distinct
+    job_code,
+    dept_code
+from
+    employee;
+
+-- 문자열 연결연산자 || 
+select
+    emp_name,
+--  salary + '원'
+    salary || '원'
+from
+    employee;
+
+select
+    12+34,
+    '12' + '34' -- 문자열이지만, 숫자로 자동형변환 처리 된 후 더하기 연산됨
+from
+    dual;
+
+----------------------------------------------------------
+-- where
+----------------------------------------------------------
+-- 특정 테이블의 행을 필터링. 특정 컬럼의 값의 조건절을 작성.
+-- boolean 으로 처리가 되며, true 가 나온 행만 결과 집합에 포함된다.
+-- 동등연산 = 
+-- 부정 동동연산 !=  <>  ^=
+-- 비교연산 > < >= <=
+-- 범위연산 between and
+-- 문자패턴 비교연산 [not] like
+-- null 여부 연산 is [not] null
+-- 포함여부 연산 [not] in
+-- 논리연산 and or
+-- 반전연산 not
+
+-- 부서코드가 D6이고 급여가 300만원보다 많은 사원의 사번, 사원명, 부서코드, 급여 조회
+select
+    emp_id, emp_name, dept_code, salary
+from
+    employee
+where
+    dept_code = 'D6' and salary > 3000000;
+
+-- 부서코드가 D5 또는 D6이고 급여가 300만원보다 많은 사원의 사번, 사원명, 부서코드, 급여 조회
+select
+    emp_id, emp_name, dept_code, salary
+from
+    employee
+where
+    (dept_code =  'D5' or dept_code = 'D6') and salary > 3000000;
+
+-- 부서코드가 D9 이 아닌 사원 조회
+select
+    *
+from
+    employee
+where
+    dept_code != 'D9';
+--  dept_code <> 'D9';
+--  dept_code ^= 'D9';
+
+-- 20년 이상 근속한 사원의 사원명과 입사일 조회
+-- 날짜 - 날짜 = 숫자(1 = 하루)
+select
+    emp_name, hire_date
+from
+    employee
+where
+    sysdate - hire_date > (365*20) 
+    and
+    quit_yn = 'N';
+
+-- 직급코드가 J1이 아닌 사원들의 급여등급(sal_level)을 중복없이 출력
+select distinct
+    sal_level
+from
+    employee
+where
+    job_code != 'J1';
+    
+-- 급여가 3500000 이상 6000000원 이하인 사원의 사원명, 급여 조회
+select
+    emp_name, salary
+from
+    employee
+where
+    salary >= 3500000 
+    and 
+    salary <= 6000000;
+
+-- between 최소값 and 최대값
+-- 최소값 이상이면서 최대값 이하인 값에 대해 true를 반환
+select
+    emp_name, salary
+from 
+    employee
+where
+    salary between 3500000 and 6000000;
+
+-- 날짜에 대한 범위 조회
+-- 입사일이 1990/01/01 ~ 2000/12/31 인 사원 조회
+select
+    emp_name, hire_date
+from 
+    employee
+where
+--    hire_date between to_date('1990/01/01') and to_date('2000/12/31');
+    hire_date between '1990/01/01' and '2000/12/31';
+
+-- 오라클이 처리 가능한 기본 날짜형식 - 다음과 같이 문자열 작성하면 날짜타입으로 처리 가능
+-- yyyy/mm/dd
+-- yyyy-mm-dd
+-- yyyymmdd
+-- yyyy mm dd
+
+-- 날짜타입은 크기비교 연산 가능
+-- 2000/01/01 이후 입사자 조회
+select
+    emp_name, hire_date
+from
+    employee
+where
+    hire_date > '2000/01/01';
+    
+-- 문자열 패턴 비교연산 like
+-- 비교하려는 컬럼값이 특정 패턴을 만족시키면 true를 반환
+-- % _ 와일드카드(파싱될 때 특수한 의미를 지니는 문자) 사용
+-- % : 0 개 이상의 문자를 의미
+-- _ : 딱 1 개의 아무 문자를 의미 
+
+-- 전씨 성을 가진 사원 조회
+select
+    emp_name
+from
+    employee
+where
+    emp_name like '전%'; -- 전으로 시작하고 0 개 이상의 문자열이 뒤따르는 값 검색. 
+
+-- 이름에 '옹'이 들어가는 사원조회
+select
+    *
+from
+    employee
+where
+    emp_name like '%옹%';
+
+-- 이메일 _ 앞글자가 3개인 이메일 조회
+select
+    email
+from 
+    employee
+where
+    email like '___\_%' escape '\'; -- escape 문자 선택은 자유롭지만, 보통 \(역슬래시) 사용할 것.
+    
+-- 전화번호 앞자리가 010 이 아닌 사원 조회
+select
+    emp_name, phone
+from 
+    employee
+where
+    phone not like '010%';
+
+-- 이메일 '_' 앞 문자가 4글자이고, 부서코드는 D9 또는 D5이면서, 입사일은 1990/01/01 ~ 2001/12/31 이고, 급여가 270만원 이상인 사원 조회
+select
+    *
+from 
+    employee
+where
+    email like '____\_%' escape '\'
+    and
+    (dept_code = 'D9' or dept_code = 'D5')
+    and
+    hire_date between '1990/01/01' and '2001/12/31'
+    and
+    salary >= 2700000;
+
+-- in 연산자 : 제시된 값 목록에 컬럼값이 포함되어 있으면 true 반환
+-- D6, D8, D9 사원 조회 
+select
+    emp_name, dept_code
+from
+    employee
+where
+--    dept_code = 'D6' or dept_code = 'D8' or dept_code = 'D9';
+    dept_code in ('D6', 'D8', 'D9');
+
+select
+    emp_name, dept_code
+from
+    employee
+where
+--    dept_code not in ('D6', 'D8', 'D9');
+    dept_code != 'D6' and dept_code <> 'D8' and dept_code ^= 'D9';
+
+-- dept_code가 null인 사원 조회 : is null
+select
+    *
+from
+    employee
+where
+    dept_code is not null;
+
+-- D6, D8 부서원과 인턴사원을 조회
+select
+    *
+from
+    employee
+where
+--    dept_code in('D6', 'D8') or dept_code is null;
+    nvl(dept_code, 'D0') in('D6', 'D8', 'D0');
+
+
+------------------------------------------------------------
+-- order by
+------------------------------------------------------------
+-- DQL 처리중 가장 마지막에 정렬 지원
+-- 특정 컬럼 기준 오름차순/내림차순, null값을 처음/마지막에 배치
+-- 컬럼명/별칭/컬럼순서를 통해 특정 컬럼 지정 가능
+
+-- 오름차순(asc) 기본값
+-- 숫자 오름차순 / 내림차순
+-- 문자열 오름차순 (사전 등재순) / 내림차순 (사전 등채 역순)
+-- 날짜셩 오름차순 (과거 ~ 미래) / 내림차순 (미래 ~ 과거)
+select
+    *
+from
+    employee
+order by
+    emp_name desc;
+
+-- 급여 내림차순
+select
+    *
+from
+    employee
+order by
+    salary desc;
+
+-- 부서 오름차순
+select
+    *
+from
+    employee
+order by
+--    dept_code asc nulls first;
+    dept_code, emp_name;
+
+-- 별칭, 컬럼 순서로 지정
+select
+    emp_id 사번,
+    emp_name 사원명,
+    salary 급여
+from
+    employee
+where
+    salary >= 2000000
+order by
+--    급여 desc;
+    2 asc;
+
+--=========================================================
+-- functions
+--=========================================================
+-- 일련의 수행 절차를 함수 객체로 만들고, 이를 호출해 사용함.
+-- SQL의 function 은 무조건 하나의 값을 반환함.
+
+-- 단일행 처리 함수 : 행별로 처리되는 함수
+    -- 1. 문자 처리 함수
+    -- 2. 숫자 처리 함수수
+    -- 3. 날짜 처리 함수
+    -- 4. 형변환 함수
+    -- 5. 기타 함
+
+-- 그룹 함수 : 여러 행을 그룹짓고, 그룹당 한번만 실행되는 함수
+
+------------------------------------------------------------
+-- 단일행 처리 함수
+------------------------------------------------------------
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- 1. 문자 처리 함수
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- length(col) : 컬럼값의 길이를 반환
+select
+    emp_name, length(emp_name),
+    email, length(email)
+from
+    employee;
+    
+-- 이메일의 길이가 15글자 미만인 사원 조회
+select
+    emp_name, email
+from
+    employee
+where
+    15 > length(email);
+
+-- instr(col, search, [start], [occurence]) : col에서 검색된 search 의 인덱스를 반환
+-- SQL의 인덱스는 1부터 시작
+select
+    instr('kh정보교육원 국가정보원 정보문화사', '정보'), -- 3
+    instr('kh정보교육원 국가정보원 정보문화사', '정보', 5), -- 11
+    instr('kh정보교육원 국가정보원 정보문화사', '정보', 5, 2), -- 15
+    instr('kh정보교육원 국가정보원 정보문화사', '정보', -1), -- 15환
+    instr('kh정보교육원 국가정보원 정보문화사', 'ㅋㅋㅋ') -- 값이 없을때에는 0을 반환
+from
+    dual;
+
+-- @실습문제 1. EMPLOYEE 테이블에서 이름, 연봉(월급*12), 총수령액(보너스포함연봉), 실수령액(총 수령액-(월급*세금 3%))가 출력되도록 하시오 (컬럼명을 지정한 별칭으로 변경)
+select 
+    emp_name 연봉,
+    (salary +(salary*nvl(bonus, 0)))*12 총수령액,
+    ((salary +(salary*nvl(bonus, 0)))*12) - (salary*0.3) 실수령액
+from
+    employee;
+
+-- @실습문제 2. EMPLOYEE 테이블에서 이름, 입사일, 근무 일수(입사한지 몇일인가)를 출력해보시오.
+select 
+    emp_name 이름,
+    hire_date 입사일,
+    sysdate - hire_date 근무일수
+from
+    employee;
+    
+-- @실습문제 3. tbl_escape_watch 테이블에서 description 컬럼에 99.99% 라는 글자가 들어있는 행만 추출하세요.
+    create table tbl_escape_watch(
+        watchname   varchar2(40)
+        ,description    varchar2(200)
+    );
+    --drop table tbl_escape_watch;
+    insert into tbl_escape_watch values('금시계', '순금 99.99% 함유 고급시계');
+    insert into tbl_escape_watch values('은시계', '고객 만족도 99.99점를 획득한 고급시계');
+    commit;
+    select * from tbl_escape_watch;
+    
+select
+    description
+from
+    tbl_escape_watch
+where
+    description like '%99.99\%%' escape '\';
+
+commit;
+
+
 
